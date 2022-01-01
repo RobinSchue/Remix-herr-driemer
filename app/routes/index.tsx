4fbaec1e829a4006e3fd4b2e630d586e2a3a7044
robin schuettert <hello@robinschuettert.com>
2022-01-01 17:58:05 +0100

    feat: poc tailwind

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,20 +2,26 @@ import { Link } from "remix";
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Moin, Ich bin Luke!</h1>
-      <p className="font-mono text-2xl">
+    <div
+      className="p-8"
+      style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}
+    >
+      <h1 className="text-3xl mb-16">Moin, Ich bin Luke!</h1>
+      <p className="font-mono mb-8">
         Weil ich zu busy bin, befindet sich diese Website aktuell noch im
         Aufbau! Sry ...
       </p>
-      <a target="_blank" href="https://www.instagram.com/driem.works">
+      <a
+        target="_blank"
+        href="https://www.instagram.com/driem.works"
+        className="mb-4"
+      >
         Instagram
       </a>
       <br />
+      <Link to="/imprint">Impressum</Link>
 
-      <footer>
-        <Link to="/imprint">Impressum</Link>
-      </footer>
+      <footer></footer>
     </div>
   );
 }
